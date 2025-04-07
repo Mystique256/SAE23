26ab89c4e10761a73fc16df778dabe2ed23841a3
Mystique256 <durpoixmatheo@gmail.com>
2025-04-07 15:36:14 +0200

    Maj Backend page de login

---
+++ traitement/T_register.php
@@ -0,0 +1,22 @@
+<?php
+// Inclusion du fichier de configuration
+require('C_config.php');
+
+// Récupération des données du formulaires
+$nom = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$add = "default" ;  
+
+if ($password != $confirm_password) {
+    echo "Les mots de passe ne correspondent pas";
+    header('Location: ../pages/login.html');
+    exit; // Add exit to prevent further execution
+} else {
+    $sql = "INSERT INTO user (nom, AdresseMail, password, Adresse) VALUES ('$nom', '$email', '$password', '$add' )";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    header('Location: ../pages/login.html');
+    exit;
+}
