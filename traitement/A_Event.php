0d9c41d06b8ffbb8ed104d5334931f232832a2d0
Mystique256 <durpoixmatheo@gmail.com>
2025-04-08 11:26:03 +0200

    Bla

--- traitement/A_Event.php
+++ traitement/A_Event.php
@@ -4,10 +4,11 @@ require_once 'C_config.php';
 $stmt = $conn->query("SELECT * FROM Trajet ORDER BY date, time");
 
 while ($trip = $stmt->fetch_assoc()) {
+    $id = $trip['id'];
     echo '<article class="trip-card">';
     echo '<div class="trip-card-header">';
     echo '<span class="trip-destination">' . htmlspecialchars($trip['destination']) . '</span>';
-    echo '<button class="join-btn">Rejoindre</button>';
+    echo '<button class="join-btn" data-trip-id="<?php echo $id ;?>"><a href="../traitement/T_Insert.php">Rejoindre</a></button>';
     echo '</div>';
     echo '<div class="trip-card-body">';
     echo '<p>' . htmlspecialchars($trip['time']) . '<br>' . htmlspecialchars($trip['date']) . '<br>départ depuis ' . htmlspecialchars($trip['departure']) . '</p>';
